f728943f94a4c5511853e8d840e0ba7764a0df57
wolegequ <460791814@qq.com>
2016-05-12 17:55:32 +0800

    架构搭建

---
+++ CSDNComment/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CSDNComment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.GetCookies = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 59);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(493, 250);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // GetCookies
+            // 
+            this.GetCookies.Location = new System.Drawing.Point(35, 12);
+            this.GetCookies.Name = "GetCookies";
+            this.GetCookies.Size = new System.Drawing.Size(75, 23);
+            this.GetCookies.TabIndex = 1;
+            this.GetCookies.Text = "button1";
+            this.GetCookies.UseVisualStyleBackColor = true;
+            this.GetCookies.Click += new System.EventHandler(this.GetCookies_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 388);
+            this.Controls.Add(this.GetCookies);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button GetCookies;
+    }
+}
+
---
+++ CSDNComment/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace CSDNComment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Url =new Uri( "http://www.csdn.net/");
+        }
+
+        private void GetCookies_Click(object sender, EventArgs e)
+        {
+            CookieContainer myCookieContainer = new CookieContainer();
+        if (webBrowser1.Document.Cookie != null)
+        {
+            string cookieStr = webBrowser1.Document.Cookie;
+            string[] cookstr = cookieStr.Split(';');
+            foreach (string str in cookstr)
+            {
+                string[] cookieNameValue = str.Split('=');
+                Cookie ck = new Cookie(cookieNameValue[0].Trim().ToString(), cookieNameValue[1].Trim().ToString());
+                ck.Domain = ".csdn.net";
+                myCookieContainer.Add(ck);
+            }
+        }
+        }
+    }
+}
